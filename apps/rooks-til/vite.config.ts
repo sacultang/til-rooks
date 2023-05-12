d593c1fb796d944870a2541b42915ddaf26ec5dc
sacultang <sacultang@gmail.com>
2023-05-12 23:07:05 +0900

    chore : vite-config-path alias 설정 패키지 설치 및 설정

--- apps/rooks-til/vite.config.ts
+++ apps/rooks-til/vite.config.ts
@@ -1,7 +1,11 @@
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
   plugins: [react()],
 })
