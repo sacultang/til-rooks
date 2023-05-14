0353a8dba4e09952b51fcbc853c2000156d194c6
sacultang <sacultang@gmail.com>
2023-05-14 15:34:10 +0900

    feat : useCountdown.ts day-3

--- packages/lib/src/hooks/useCountdown.ts
+++ packages/lib/src/hooks/useCountdown.ts
@@ -1,1 +1,37 @@
 import { useState } from 'react'
+import { useIntervalWhen } from './useIntervalWhen'
+
+type CountdownOptions = {
+  interval?: number
+  onDown?: (restTime: number, newTime: Date) => void
+  onEnd?: (newTime: Date) => void
+}
+
+function useCountdown(endTime: Date, options: CountdownOptions = {}): number {
+  const { interval = 1_000, onDown, onEnd } = options
+  const [time, setTime] = useState<Date>(() => new Date())
+  const restTime = endTime.getTime() - time.getTime()
+  const count = restTime > 0 ? Math.ceil(restTime / interval) : 0
+  const onTick = () => {
+    const newTime = new Date()
+    if (newTime > endTime) {
+      if (onEnd) {
+        onEnd(newTime)
+      }
+
+      setTime(endTime)
+      return
+    }
+
+    if (onDown) {
+      onDown(restTime, newTime)
+    }
+
+    setTime(newTime)
+  }
+  useIntervalWhen(onTick, count ? interval : undefined, true, true)
+
+  return count
+}
+
+export { useCountdown }
--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,2 +1,3 @@
 export { useArrayState } from './hooks/useArrayState'
 export { useIntervalWhen } from './hooks/useIntervalWhen'
+export { useCountdown } from './hooks/useCountdown'
