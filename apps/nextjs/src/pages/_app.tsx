5fa1d2c2ad0167c836aa5e9ea6149cd1b42e9f9e
sacultang <sacultang@gmail.com>
2023-05-20 00:56:01 +0900

    feat : navbar styling

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,5 +1,20 @@
+/* eslint-disable react/no-unknown-property */
 import type { AppProps } from 'next/app'
 import '@/scss/index.scss'
+import { Raleway, Fira_Code } from 'next/font/google'
+const raleway = Raleway({ subsets: ['latin'] })
+const firecode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --raleway: ${raleway.style.fontFamily};
+          --fira-code: ${firecode.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
